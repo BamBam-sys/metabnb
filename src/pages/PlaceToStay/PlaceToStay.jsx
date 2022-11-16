6d468240a4f87bcff5a0f004685d3f790a01dcf2
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-16 03:37:48 +0100

    create placesToStay page

--- src/pages/PlaceToStay/PlaceToStay.jsx
+++ src/pages/PlaceToStay/PlaceToStay.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import styles from './placeToStay.module.scss';
+import setting from '../../assets/setting.png';
+import data from '../../data.json';
+import { Card } from '../../common';
+import { IoMenu, IoClose } from 'react-icons/io5';
+import { useState } from 'react';
 
 const PlaceToStay = () => {
-  return <div>PlaceToStay</div>;
+  const [dropDown, setDropDown] = useState(false);
+
+  const handleDropdown = () => setDropDown((prev) => !prev);
+
+  return (
+    <div className={`${styles.placeToStay} container`}>
+      <div className={styles.header}>
+        {dropDown ? (
+          <IoClose className={styles.icon} onClick={() => handleDropdown()} />
+        ) : (
+          <IoMenu className={styles.icon} onClick={() => handleDropdown()} />
+        )}
+
+        <div className={styles.links}>
+          <span>Restaurant</span>
+          <span>Cottage</span>
+          <span>Fantasy city</span>
+          <span>Beach</span>
+          <span>Carbins</span>
+          <span>Off-grid</span>
+          <span>Farm</span>
+        </div>
+
+        {/* mobile */}
+        {dropDown && (
+          <div className={styles.mobileLinks}>
+            <span>Restaurant</span>
+            <span>Cottage</span>
+            <span>Fantasy city</span>
+            <span>Beach</span>
+            <span>Carbins</span>
+            <span>Off-grid</span>
+            <span>Farm</span>
+          </div>
+        )}
+        <div className={styles.setting}>
+          location <img src={setting} alt="setting" />
+        </div>
+      </div>
+
+      <div className={styles.cards}>
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PlaceToStay;
